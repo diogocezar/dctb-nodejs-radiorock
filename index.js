8342904e24bf820f7946c1074f7d1b99f330cdec
Diogo Cezar <diogo@diogocezar.com>
2018-05-26 03:22:15 -0300

    Implementação Geral

--- index.js
+++ index.js
@@ -1,29 +1,79 @@
-const https = require('https')
-const fs = require('fs')
-const url = 'https://players.gc2.com.br/cron/89fm/results2.json'
-const fileName =  './data.json'
-const file = require(fileName)
+const https    = require('https')
+const fs       = require('fs')
 
-https.get(url, (res) => {
-    var data = '';
-    res.on('data', (chunk) => {
-        data += chunk;
-        console.log(data);
-    });
-    res.on('end', () => {
-        var json = JSON.parse(data);
-        console.log("Got a response: ", json);
-        const music = json.musicas.tocando
+class RadioRock {
+    constructor(){
+        this.url      = 'https://players.gc2.com.br/cron/89fm/results2.json'
+        this.fileName = './data.json'
+        this.file     = require(this.fileName)
+        this.data     = ''
+        this.seconds  = 60
+        console.log('Iniciando aplicativo.');
+        this.loop()
+    }
+    loop(){
+        this.getData()
+        setTimeout(() => {
+            this.getData()
+            this.loop()
+        }, this.seconds*1000)
+    }
+    getData(){
+        console.log('Iniciando a busca pelo JSON.')
+        https.get(this.url, (res)  => {
+            this.data = ''
+            res.on('data', (chunk) => { 
+                console.log('Recebendo data.')
+                this.data += chunk; 
+            });
+            res.on('end', ()       => { 
+                console.log('JSON retornado.')
+                this.writeFile(this.getJsonResult(this.data)) 
+            });
+        }).on('error', function (err) {
+            return console.log(err);
+        });
+    }
+    getJsonResult(json){
+        console.log('Realizando parse do JSON.')
+        json = JSON.parse(json.replace(/'/g, ''))
+        const music  = json.musicas.tocando
         const singer = music.singer
-        const song = music.tocando
-        file.song = {
+        const song   = music.song
+        return {
             'singer' : singer,
-            'song' : song
+            'song'   : song
         }
-        fs.writeFile(fileName, JSON.stringify(file), (err) => {
-            if(err) console.log('Error on write file.');
-        })
-    });
-}).on('error', function (e) {
-    console.log("Got an error: ", e);
-});
+    }
+    writeFile(result){
+        console.log('Abrindo base já existente.')
+        fs.readFile(this.fileName, (err, oldContent) => {
+            if (err) { return console.log(err) }
+            oldContent = JSON.parse(oldContent)
+            if(this.checkIfExists(result, oldContent)){
+                console.log('\x1b[32m', '')
+                console.log(`Nova música encontrada: ${result.singer} - ${result.song}`)
+                console.log('\x1b[0m')
+                oldContent[new Date().toLocaleString()] = result
+                fs.writeFile(this.fileName, JSON.stringify(oldContent), (err) => {
+                    if (err) { return console.log(err) }
+                })
+            }
+            else{
+                console.log('\x1b[33m', '')
+                console.log(`Música já encontrada: ${result.singer} - ${result.song}`)
+                console.log('\x1b[0m')
+            }
+            console.log('Aguardando...')
+        });        
+    }
+    checkIfExists(result, dataBase){
+        for (let k in dataBase) {
+            if(dataBase[k].singer == result.singer && dataBase[k].song == result.song)
+                return false;
+        }
+        return true;
+    }
+}
+
+new RadioRock();
