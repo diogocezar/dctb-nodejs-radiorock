77d44d5c7140852c476e81b67d9228f2651be4ca
Diogo Cezar <diogo@diogocezar.com>
2018-05-27 02:06:00 -0300

    Ajustes FireBase

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
-const https    = require('https')
-const fs       = require('fs')
+const https   = require('https')
+const fs      = require('fs')
+const fb      = require("firebase-admin");
 
 class RadioRock {
     constructor(){
         this.url      = 'https://players.gc2.com.br/cron/89fm/results2.json'
+        this.dbUrl    = 'https://dctb-radiorock.firebaseio.com'
+        this.keyFile  = './key.json'
         this.fileName = './data.json'
         this.file     = require(this.fileName)
+        this.account  = require(this.keyFile)
         this.data     = ''
         this.seconds  = 60
-        console.log('Iniciando aplicativo.');
+        this.configureFirebase()
         this.loop()
     }
     loop(){
@@ -17,6 +21,13 @@ class RadioRock {
             this.getData()
             this.loop()
         }, this.seconds*1000)
+    }
+    configureFirebase(){
+        console.log('Configurando conexão com o banco de dados FireBase.')
+        fb.initializeApp({
+            credential: fb.credential.cert(this.account),
+            databaseURL: this.dbUrl
+        });
     }
     getData(){
         console.log('Iniciando a busca pelo JSON.')
@@ -28,7 +39,9 @@ class RadioRock {
             });
             res.on('end', ()       => { 
                 console.log('JSON retornado.')
-                this.writeFile(this.getJsonResult(this.data)) 
+                const json = this.getJsonResult(this.data)
+                this.writeFile(json)
+                this.writeFireBase(json)
             });
         }).on('error', function (err) {
             return console.log(err);
@@ -45,14 +58,45 @@ class RadioRock {
             'song'   : song
         }
     }
+    writeFireBase(result){
+        console.log('Gravação no FireBase.')
+        const obj = {}
+        obj[new Date().toLocaleString()] = result
+        this.checkIfExistsFb(result, obj);
+    }
+    checkIfExistsFb(result, obj){
+        const db  = fb.database();
+        const ref = db.ref("/radio-rock");
+        ref.on("value", (snapshot) => {
+            const dataBase = snapshot.val();
+            let save       = true;
+            for (let k in dataBase) {
+                if (dataBase[k].singer == result.singer && dataBase[k].song == result.song){
+                    console.log('\x1b[33m', '')
+                    console.log(`[FIREBASE] Música já encontrada: ${result.singer} - ${result.song}`)
+                    console.log('\x1b[0m')
+                    save = false
+                }
+            }
+            if(save){
+                console.log('\x1b[32m', '')
+                console.log(`[FIREBASE] Nova música encontrada: ${result.singer} - ${result.song}`)
+                console.log('\x1b[0m')
+                ref.update(obj)
+            }
+                
+        }, function (errorObject) {
+            console.log("The read failed: " + errorObject.code);
+        });
+    }
     writeFile(result){
-        console.log('Abrindo base já existente.')
+        console.log('Gravação em Arquivo Local.')
         fs.readFile(this.fileName, (err, oldContent) => {
             if (err) { return console.log(err) }
             oldContent = JSON.parse(oldContent)
             if(this.checkIfExists(result, oldContent)){
                 console.log('\x1b[32m', '')
-                console.log(`Nova música encontrada: ${result.singer} - ${result.song}`)
+                console.log(`[FILE] Nova música encontrada: ${result.singer} - ${result.song}`)
                 console.log('\x1b[0m')
                 oldContent[new Date().toLocaleString()] = result
                 fs.writeFile(this.fileName, JSON.stringify(oldContent), (err) => {
@@ -61,7 +105,7 @@ class RadioRock {
             }
             else{
                 console.log('\x1b[33m', '')
-                console.log(`Música já encontrada: ${result.singer} - ${result.song}`)
+                console.log(`[FILE] Música já encontrada: ${result.singer} - ${result.song}`)
                 console.log('\x1b[0m')
             }
             console.log('Aguardando...')
